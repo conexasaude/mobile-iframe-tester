1d1f0dfc20f2987601253fd7d9ace571708a3b41
Caio Nilton Lemos Barroso <caioniltonlemosbarroso@gmail.com>
2024-04-17 22:40:15 -0300

    removing themeprovider

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import React, {useContext} from 'react';
-import {ThemeContext, ThemeProviderLocal} from './theme';
-import {ThemeProvider} from '@conexasaude/styles';
+// import {ThemeContext, ThemeProviderLocal} from './theme';
+// import {ThemeProvider} from '@conexasaude/styles';
 
 import {IframeTester} from './src';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  const {theme} = useContext(ThemeContext);
+  // const {theme} = useContext(ThemeContext);
 
   return (
-    <ThemeProviderLocal>
-      <ThemeProvider theme={theme}>
-        <IframeTester />
-      </ThemeProvider>
-    </ThemeProviderLocal>
+    // <ThemeProviderLocal>
+    //   <ThemeProvider theme={theme}>
+    <IframeTester />
+    //   </ThemeProvider>
+    // </ThemeProviderLocal>
   );
 };
 
--- theme.tsx
+++ theme.tsx
@@ -1,31 +1,42 @@
-import React, {createContext, useEffect, useState} from 'react';
-import {Theme, theme as heroTheme} from '@conexasaude/styles';
+// import React, {createContext, useEffect, useState} from 'react';
+// import {Theme, theme as heroTheme} from '@conexasaude/styles';
 
-export const ThemeContext = createContext({
-  changeColors: () => {},
-  theme: heroTheme,
-});
+// export const ThemeContext = createContext({
+//   changeColors: () => {},
+//   theme: heroTheme,
+// });
 
-const ThemeProviderLocal = ({children}: any) => {
-  const [heroThemeState, setHeroThemeState] = useState<Theme>(heroTheme);
+// const ThemeProviderLocal = ({children}: any) => {
+//   const [heroThemeState, setHeroThemeState] = useState<Theme>(heroTheme);
 
-  useEffect(() => {
-    changeColors();
-  }, []);
+//   useEffect(() => {
+//     changeColors();
+//   }, []);
 
-  const changeColors = async () => {
-    setHeroThemeState(heroTheme);
-  };
+//   const changeColors = async () => {
+//     setHeroThemeState(heroTheme);
+//   };
 
-  return (
-    <ThemeContext.Provider
-      value={{
-        changeColors: () => changeColors(),
-        theme: heroThemeState,
-      }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+//   return (
+//     <ThemeContext.Provider
+//       value={{
+//         changeColors: () => changeColors(),
+//         theme: heroThemeState,
+//       }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// };
+
+// export {ThemeProviderLocal};
+
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const react_native_iframe_tester: React.FC = () => {
+  return <View />;
 };
 
-export {ThemeProviderLocal};
+export default react_native_iframe_tester;
